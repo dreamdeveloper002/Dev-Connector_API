16b777f969cc69bd7eda0cc8f79b85a436fe8c41
dreamdeveloper002 <bkosemani1@gmail.com>
2021-01-07 23:51:23 +0100

    create post controller

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,15 +1,51 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../../models/Post');
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
 
 
 
-//@route GET api/posts
-//@desc Test route
-//@access Public(no token required)
-router.get('/', (req, res) => {
-    res.send('Post route');
-})
+//@route Post api/posts
+//@desc  Create a post
+//@access Private(token required)
+router.post('/', [ auth,[ 
+    check('text', 'Text is required')
+    .not()
+    .isEmpty(),
+]], async (req, res) => {
 
+    console.log(req.body)
+    const errors = validationResult(req);
 
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    };
+    
+
+    try {
+
+     const user = await User.findById(req.user.id)
+
+     const newPost = new Post({
+         text: req.body.text,
+         name: user.name,
+         avatar: user.avatar,
+         user: req.user.id
+     })
+
+     const post = await newPost.save();
+
+     res.json(post);
+        
+    } catch (err) {
+        console.error(err.message); 
+        return res.status(500).send({ message: "Server error"});  
+        
+    }
+});
+
+ 
 
 module.exports = router
